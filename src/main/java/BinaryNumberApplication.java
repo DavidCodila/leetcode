6906e770e74f3af6969d19af79cfa55c2be583b4
David Codila <davidcodila@192-168-1-105.tpgi.com.au>
2025-01-13 17:27:57 +1100

    Add tests for 7 and 11 and alter the code

--- src/main/java/BinaryNumberApplication.java
+++ src/main/java/BinaryNumberApplication.java
@@ -3,9 +3,9 @@ public class BinaryNumberApplication {
     }
     public String hasAlternatingBits(int number) {
         String numberAsABinaryString = Integer.toBinaryString(number);
-        String hasAlternating = "false";
-        if (numberAsABinaryString.contains("10") || numberAsABinaryString.contains("01")) {
-            hasAlternating = "true";
+        String hasAlternating = "true";
+        if (numberAsABinaryString.contains("11") || numberAsABinaryString.contains("00")) {
+            hasAlternating = "false";
         }
         return "Output: " + hasAlternating + "\n" +
                 "Explanation: The binary representation of " + number + " is: " + numberAsABinaryString;
--- src/test/java/BinaryNumberApplicationTest.java
+++ src/test/java/BinaryNumberApplicationTest.java
@@ -10,4 +10,20 @@ public class BinaryNumberApplicationTest {
                 "Explanation: The binary representation of 5 is: 101";
         assertEquals(expectedOutput, sut.hasAlternatingBits(5));
     }
+
+    @Test
+    public void testBinaryNumberFor7() {
+        BinaryNumberApplication sut = new BinaryNumberApplication();
+        String expectedOutput = "Output: false\n" +
+                "Explanation: The binary representation of 7 is: 111";
+        assertEquals(expectedOutput, sut.hasAlternatingBits(7));
+    }
+
+    @Test
+    public void testBinaryNumberFor11() {
+        BinaryNumberApplication sut = new BinaryNumberApplication();
+        String expectedOutput = "Output: false\n" +
+                "Explanation: The binary representation of 11 is: 1011";
+        assertEquals(expectedOutput, sut.hasAlternatingBits(11));
+    }
 }
