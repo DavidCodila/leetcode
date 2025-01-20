b766037e6c74f07c8117add58bef32445eea626a
David Codila <davidcodila@192-168-1-201.tpgi.com.au>
2025-01-20 14:25:39 +1100

    Add passing code for single input

--- src/main/java/ReorganizeStringApplication.java
+++ src/main/java/ReorganizeStringApplication.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public final class ReorganizeStringApplication {
 
     public static String reorganizeString(String input) {
+        List<Character> charList = new ArrayList<>(input.length());
+        for (int i = 0; i < input.length(); i++) {
+            charList.add(input.charAt(i));
+        }
+        if (charList.size() == 1) {
+            return input;
+        }
         if (Objects.equals(input, "ab")) {
             return input;
-        }   
-        return "a";
+        }
+        return "";
     }
 
 }
