8a004cc77311ea26080d9c69560bc806ee272ce8
David Codila <david.codila@auspost.com.au>
2025-01-21 16:49:42 +1100

    Refactor to account for edgecases

--- src/main/java/ReorganizeStringApplication.java
+++ src/main/java/ReorganizeStringApplication.java
@@ -1,15 +1,19 @@
+import java.security.InvalidParameterException;
+
 public final class ReorganizeStringApplication {
 
     public static String reorganizeString(String input) {
-        if (input.length() == 1)
-            return input;
-        if (input.length() == 2) {
-            if (input.charAt(0) == input.charAt(1))
-                return "";
-            else
-                return input;
-        }
+        validateInput(input);
         CustomCharacterArrayClass customCharacterArrayClass = new CustomCharacterArrayClass(input);
         return customCharacterArrayClass.reorganizeWihNoRepeatingCharacters();
+    }
+
+    private static void validateInput(String input) {
+        if (input.isEmpty())
+            throw new InvalidParameterException("Input can not be empty");
+        if (input.length() > 500)
+            throw new InvalidParameterException("Input can not exceed 500 characters");
+        if (!input.equals(input.toLowerCase()))
+            throw new InvalidParameterException("Input must consist of only lowercase letters");
     }
 }
--- src/test/java/ReorganizeStringApplicationTest.java
+++ src/test/java/ReorganizeStringApplicationTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
+
+import java.security.InvalidParameterException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReorganizeStringApplicationTest {
 
@@ -46,6 +50,31 @@ public class ReorganizeStringApplicationTest {
     @Test
     public void testStringReorganizeWithAAAA() {
         assertEquals("", ReorganizeStringApplication.reorganizeString("aaaa"));
+    }
+
+    @Test
+    public void testStringReorganizeWithEmptyInput() {
+        InvalidParameterException exception =
+                assertThrows(InvalidParameterException.class, () -> ReorganizeStringApplication.reorganizeString("")
+        );
+        assertEquals(exception.getMessage(), "Input can not be empty");
+    }
+
+    @Test
+    public void testStringReorganizeWithOver500CharacterInput() {
+        String largeInput = String.valueOf(new char[501]);
+        InvalidParameterException exception =
+                assertThrows(InvalidParameterException.class, () -> ReorganizeStringApplication.reorganizeString(largeInput)
+        );
+        assertEquals(exception.getMessage(), "Input can not exceed 500 characters");
+    }
+
+    @Test
+    public void testStringReorganizeWithUppercaseABBCInput() {
+        InvalidParameterException exception =
+                assertThrows(InvalidParameterException.class, () -> ReorganizeStringApplication.reorganizeString("ABBC")
+        );
+        assertEquals(exception.getMessage(), "Input must consist of only lowercase letters");
     }
 
 }
