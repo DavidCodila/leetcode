42b655e0242cfb672c6542640ad1fb51ec907b3b
David Codila <david.codila@auspost.com.au>
2025-01-22 14:35:24 +1100

    Make reorganizeString a static helper funtion

--- src/main/java/ReorganizeStringApplication.java
+++ src/main/java/ReorganizeStringApplication.java
@@ -4,8 +4,66 @@ public final class ReorganizeStringApplication {
 
     public static String reorganizeString(String input) {
         validateInput(input);
-        CustomCharacterArrayClass customCharacterArrayClass = new CustomCharacterArrayClass(input);
-        return customCharacterArrayClass.reorganizeWihNoRepeatingCharacters();
+        char[] characterArray = input.toCharArray();
+        while (getRepeatedCharIndex(characterArray) != -1) {
+            characterArray = getReorganizedArray(characterArray);
+        }
+        return String.valueOf(characterArray);
+    }
+
+
+    private static int getRepeatedCharIndex(char[] characterArray) {
+        for (int i = 0; i < characterArray.length - 1; i++) {
+            if (characterArray[i] == characterArray[i + 1])
+                return i;
+        }
+        return -1;
+    }
+
+    private static char[] getReorganizedArray(char[] characterArray) {
+        for (int i = 0; i < characterArray.length - 1; i++) {
+            if (indexToShiftCharacterExists(i, characterArray)) {
+                switch (getCharacterShiftDirection(i, characterArray)) {
+                    case "left":
+                        return shiftCharacterToTheLeft(i, characterArray);
+                    case "right":
+                        return shiftCharacterToTheRight(i, characterArray);
+                }
+            }
+        }
+        return new char[]{}; //returns empty char array if the array can not be rearranged
+    }
+
+    private static boolean indexToShiftCharacterExists(int indexToShiftCharacterTo, char[] characterArray) {
+        char characterToMove = characterArray[getRepeatedCharIndex(characterArray)];
+        return characterToMove != characterArray[indexToShiftCharacterTo]
+                && characterToMove != characterArray[indexToShiftCharacterTo + 1];
+    }
+
+    private static String getCharacterShiftDirection(int indexToShiftCharacterTo, char[] characterArray) {
+        if (indexToShiftCharacterTo < getRepeatedCharIndex(characterArray))
+            return "left";
+        return "right";
+    }
+
+    private static char[] shiftCharacterToTheLeft(int indexToShiftCharacterTo, char[] characterArray) {
+        int repeatedCharacterIndex = getRepeatedCharIndex(characterArray);
+        char characterToMove = characterArray[repeatedCharacterIndex];
+        for (int j = repeatedCharacterIndex; j > indexToShiftCharacterTo - 1; j--) {
+            characterArray[j + 1] = characterArray[j];
+        }
+        characterArray[indexToShiftCharacterTo + 1] = characterToMove;
+        return characterArray;
+    }
+
+    private static char[] shiftCharacterToTheRight(int indexToShiftCharacterTo, char[] characterArray) {
+        int repeatedCharacterIndex = getRepeatedCharIndex(characterArray);
+        char characterToMove = characterArray[repeatedCharacterIndex];
+        for (int j = repeatedCharacterIndex; j < indexToShiftCharacterTo; j++) {
+            characterArray[j] = characterArray[j + 1];
+        }
+        characterArray[indexToShiftCharacterTo] = characterToMove;
+        return characterArray;
     }
 
     private static void validateInput(String input) {
