be8bc8ab88452dad632fb8c9e03968705d0d8f99
David Codila <david.codila@auspost.com.au>
2025-01-28 12:49:44 +1100

    Add code to make findEvenNumberOfDigits method work

--- src/main/java/FindNumbers.java
+++ src/main/java/FindNumbers.java
@@ -1,5 +1,15 @@
 public class FindNumbers {
     public static int findEvenNumberOfDigits(int[] nums) {
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        int evenNumberCount = 0;
+        for (int num : nums) {
+            int numberLength = String.valueOf(num).replaceFirst("-","").length();
+            if (numberLength % 2 == 0) {
+                evenNumberCount++;
+            }
+        }
+        return evenNumberCount;
     }
 }
--- src/test/java/FindNumbersTest.java
+++ src/test/java/FindNumbersTest.java
@@ -4,13 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindNumbersTest {
     @Test
+    public void TestFindEvenNumberOfDigitsWithEmptyArray() {
+        assertEquals(0, FindNumbers.findEvenNumberOfDigits(new int[] { }));
+    }
+
+    @Test
+    public void TestFindEvenNumberOfDigits0() {
+        assertEquals(0, FindNumbers.findEvenNumberOfDigits(new int[] { 0 }));
+    }
+
+    @Test
     public void TestFindEvenNumberOfDigits1() {
         assertEquals(0, FindNumbers.findEvenNumberOfDigits(new int[] { 1 }));
     }
 
     @Test
     public void TestFindEvenNumberOfDigits12() {
-        assertEquals(1, FindNumbers.findEvenNumberOfDigits(new int[] { 1, 2 }));
+        assertEquals(1, FindNumbers.findEvenNumberOfDigits(new int[] { 11 }));
+    }
+
+    @Test
+    public void TestFindEvenNumberOfDigits12_345_2_6_7896() {
+        assertEquals(2, FindNumbers.findEvenNumberOfDigits(new int[] { 12, 345, 2, 6, 7896 }));
+    }
+
+    @Test
+    public void TestFindEvenNumberOfDigitsNegative11() {
+        assertEquals(1, FindNumbers.findEvenNumberOfDigits(new int[] { -11 }));
     }
 
 }
