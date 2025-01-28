9bdd9b48ad56854bfc80ad1ab66ab6723e160d64
David Codila <david.codila@auspost.com.au>
2025-01-29 10:28:50 +1100

    Refactor binarySearch code for readability

--- src/main/java/Binary.java
+++ src/main/java/Binary.java
@@ -4,15 +4,14 @@ public class Binary {
             return -1;
         int start = 0;
         int end = nums.length - 1;
-        int mid;
         while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (nums[mid] == target)
-                return mid;
-            if (nums[mid] > target)
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target)
+                start = mid + 1;
+            else if (nums[mid] > target)
                 end = mid - 1;
             else
-                start = mid + 1;
+                return mid;
         }
         return -1;
     }
