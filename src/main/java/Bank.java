4380143fe2de04931ff022e201130f015992d6d5
David Codila <david.codila@auspost.com.au>
2025-01-28 15:01:37 +1100

    Add passing code for getRichestCustomerWealth

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,5 +1,21 @@
 public class Bank {
-    public static int getRichestCustomer(int[][] accounts) {
-        return 0;
+    public static int getRichestCustomerWealth(int[][] arrayOfBankAccounts) {
+        if (arrayOfBankAccounts.length == 0)
+            return 0;
+        int maxWealth = 0;
+        for (int[] BankAccounts : arrayOfBankAccounts) {
+            int customerWealth = getTotalWealth(BankAccounts);
+            if (customerWealth > maxWealth)
+                maxWealth = customerWealth;
+        }
+        return maxWealth;
+    }
+
+    private static int getTotalWealth(int[] bankAccounts) {
+        int wealth = 0;
+        for (int bankAccount : bankAccounts) {
+            wealth += bankAccount;
+        }
+        return wealth;
     }
 }
--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -5,11 +5,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
     @Test
-    public void TestGetRichestCustomer_11_() {
+    public void TestGetRichestCustomerEmpty() {
+        assertEquals(
+                0,
+                Bank.getRichestCustomerWealth(
+                        new int[][]{ { } }
+                )
+        );
+    }
+
+    @Test
+    public void TestGetRichestCustomer_11() {
+        assertEquals(
+                2,
+                Bank.getRichestCustomerWealth(
+                        new int[][]{ { 1, 1 } }
+                )
+        );
+    }
+
+    @Test
+    public void TestGetRichestCustomer_11_22_33() {
         assertEquals(
                 6,
-                Bank.getRichestCustomer(
-                        new int[][]{ { 1, 1 } }
+                Bank.getRichestCustomerWealth(
+                        new int[][]{ { 1, 1 }, { 2, 2 }, { 3, 3 } }
+                )
+        );
+    }
+
+    @Test
+    public void TestGetRichestCustomerNegative_11() {
+        assertEquals(
+                0,
+                Bank.getRichestCustomerWealth(
+                        new int[][]{ { -1, -1 } }
                 )
         );
     }
