f9150f902497d4a62a98818f26dd4496489c9eae
David Codila <david.codila@auspost.com.au>
2025-01-28 14:31:14 +1100

    Refactor findEvenNumberOfDigits method to use log10 instead

--- src/main/java/FindNumbers.java
+++ src/main/java/FindNumbers.java
@@ -5,11 +5,20 @@ public class FindNumbers {
         }
         int evenNumberCount = 0;
         for (int num : nums) {
-            int numberLength = String.valueOf(num).replaceFirst("-","").length();
-            if (numberLength % 2 == 0) {
+            if (isEvenNumberOfDigits(num)) {
                 evenNumberCount++;
             }
         }
         return evenNumberCount;
+    }
+
+    private static boolean isEvenNumberOfDigits(int num) {
+        return getNumberOfDigits(num) % 2 == 0;
+    }
+
+    private static int getNumberOfDigits(int num) {
+        if (num < 0)
+            num = num * - 1;
+        return (int) Math.log10(num) + 1;
     }
 }
